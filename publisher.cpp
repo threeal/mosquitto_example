76840fa30ce68080463d4c6bed807225fb81c311
Alfi Maulana <alfi.maulana.f@gmail.com>
2020-12-01 23:03:11 +0700

    add subscriber and fix things

--- publisher.cpp
+++ publisher.cpp
@@ -3,8 +3,10 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 static bool alive = true;
+static bool connected = false;
 
 void handle_signal(int s)
 {
@@ -14,38 +16,46 @@ void handle_signal(int s)
 void connect_callback(struct mosquitto *mosq, void *obj, int result)
 {
   printf("Connected to MQTT broker\n");
-}
-
-void message_callback(struct mosquitto *mosq, void *obj,
-    const struct mosquitto_message *message)
-{
-  if (!strcmp(message->topic, "example/test")) {
-    printf("Received: %s\n", (char *)message->payload);
-  }
+  connected = true;
 }
 
 int main(int argc, char *argv[])
 {
+  int count = 1;
+
   signal(SIGINT, handle_signal);
   signal(SIGTERM, handle_signal);
 
   mosquitto_lib_init();
 
-  struct mosquitto *mosq = mosquitto_new("subscriber", true, NULL);
+  struct mosquitto *mosq = mosquitto_new("publisher", true, NULL);
 
   if (mosq)
   {
     mosquitto_connect_callback_set(mosq, connect_callback);
-    mosquitto_message_callback_set(mosq, message_callback);
 
     int rc = mosquitto_connect(mosq, "localhost", 1883, 60);
-
-    mosquitto_subscribe(mosq, NULL, "example/test", 0);
 
     while (alive)
     {
       if (alive && rc) {
+        connected = false;
         mosquitto_reconnect(mosq);
+      }
+
+      if (connected)
+      {
+        char buffer[32];
+        sprintf(buffer, "Hello world! (%d)", count++);
+
+        rc = mosquitto_publish(
+          mosq, NULL, "example/test", sizeof(buffer), buffer, 0, true
+        );
+
+        if (!rc) {
+          printf("Published: %s\n", buffer);
+          sleep(1);
+        }
       }
 
       rc = mosquitto_loop(mosq, -1, 1);
